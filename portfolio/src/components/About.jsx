f2c952a8f7d2111f6356db567fdcbbaf8f6901f2
koushikkumarkadari <139983437+koushikkumarkadari@users.noreply.github.com>
2025-06-17 23:24:54 +0530

    fix: update About component to include resume download link and improve text formatting

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -4,10 +4,22 @@ export default function About() {
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-6">About Me</h2>
         <img src="https://i.ibb.co/vxJV32rq/1715188352121.png" alt="Profile picture of John Doe" className="w-32 h-32 rounded-full mx-auto mb-6" />
-        <p className="text-lg">
+        <p className="text-lg  ">
           I'm a passionate web developer with expertise in creating responsive and interactive websites.
           Skilled in modern frontend and backend technologies like React, Tailwind CSS, MongoDB, ExpressJS, NodeJs, I love to turning
           ideas into visually appealing and functional digital experiences.
+        </p>
+        <p className="text-lg md:text-xl leading-relaxed">
+          Want to know more about my experience?{' '}
+          <a
+            href="https://drive.google.com/file/d/1Ka8rv-4w9x1rEurnuG-91l-18tFbkB5B/view?usp=sharing"
+            download
+            className="text-blue-600 hover:underline font-semibold"
+            aria-label="Download my resume"
+          >
+            Download my resume
+          </a>{' '}
+          to see my full background and qualifications.
         </p>
       </div>
     </section>
